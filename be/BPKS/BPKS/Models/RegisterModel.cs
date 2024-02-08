a1d40223aaab03031db28494b9af82b67062b2e2
Yu282002 <yoshino291@gmail.com>
2024-02-08 20:04:37 +0700

    fix structure

--- be/BPKS/BPKS/Models/RegisterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿public class RegisterModel
-{
-    public string UserName { get; set; }
-    public string Password { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public string AvatarUrl { get; set; }
-}
---
+++ be/BPKS/README.md
@@ -0,0 +1,9 @@
+# ASP.NET Core 4.8 project from TEDU
+## Technologies
+- ASP.NET Core 4.8
+- Entity Framework Core 4.8
+- version 8.0.100
+## Install Package
+- Microsoft.EntityFrameworkCore.Sql
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
