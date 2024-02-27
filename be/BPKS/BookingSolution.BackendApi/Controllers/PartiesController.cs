9ad2a81941c5675c4940f023cff2731e6f01f557
Yu282002 <yoshino291@gmail.com>
2024-02-27 15:35:23 +0700

    User

---
+++ be/BPKS/BookingSolution.BackendApi/Controllers/PartiesController.cs
@@ -0,0 +1,24 @@
+﻿using Booking.Application.Catalog.Parties;
+using Booking.Application.Catalog.Products;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSolution.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartiesController : ControllerBase
+    {
+        private readonly IManagePartyService _managePartyService;
+        public PartiesController(IManagePartyService managePartyService)
+        {
+            _managePartyService = managePartyService;
+        }
+        //[HttpGet]
+        //public async Task<IActionResult> Get()
+        //{
+        //    var products = await _publicProductService.GetAll();
+        //    return Ok(products);
+
+    } 
+}
