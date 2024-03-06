eaca0465b58cc7a150970f939020586e49af9097
hungnvse150584 <74083705+hungnvse150584@users.noreply.github.com>
2024-03-06 15:50:01 +0700

    Change Layout_2

---
+++ be/BPKS/Booking.AdminApp/Controllers/PartyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.AdminApp.Controllers
+{
+    public class PartyController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
