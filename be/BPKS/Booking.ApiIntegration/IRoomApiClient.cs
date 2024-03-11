cb9fc4b45fff0f9182aacf23f90d987b88ff8fff
nguyetntmse161788 <nguyetthm02@gmail.com>
2024-03-12 00:45:34 +0700

    fe room,fe search party

---
+++ be/BPKS/Booking.ApiIntegration/IRoomApiClient.cs
@@ -0,0 +1,29 @@
+﻿using BookingSolution.ViewModels.Catalog.Products;
+using BookingSolution.ViewModels.Catalog.Rooms;
+using BookingSolution.ViewModels.Common;
+
+namespace Booking.ApiIntegration
+{
+    public interface IRoomApiClient
+    {
+        Task<PagedResult<RoomVm>> GetPagings(GetManageRoomPagingRequest request);
+
+        Task<PagedResult<RoomVm>> GetPagingsParentRoom(GetManageRoomPagingRequest request);
+
+        Task<bool> CreateRoom(RoomCreateRequest request);
+
+        Task<bool> UpdateRoom(RoomUpdateRequest request);
+
+        //Task<ApiResult<bool>> CategoryAssign(int id, CategoryAssignRequest request);
+
+        Task<RoomVm> GetById(int id);
+
+        //NhậnSản phẩm nổi bật
+        Task<List<RoomUpdateRequest>> GetFeaturedRooms(int take);
+
+        //Nhận sản phẩm mới nhất
+        Task<List<RoomVm>> GetLatestRooms(int take);
+
+        Task<bool> DeleteRoom(int id);
+    }
+}
